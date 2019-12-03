4f401c3f168bfe98e17ede9cd9dbf79235732f63
Yvonne Qian <yq2278@columbia.edu>
2019-12-03 18:45:03 +0000

    Second attempt to export. Try to find how to transfer QuerySet into String.

--- mysite/management/commands/export_squirrel_data.py
+++ mysite/management/commands/export_squirrel_data.py
@@ -11,6 +11,12 @@ class Command(BaseCommand):
     def handle(self, *args,**options):
         csvpath = args[0]
         with open(csvpath,'w') as output:
-            data = Squirrel()
+            data = Squirrel.objects.all()
+            header = [['X','Y','Unique Squirrel ID','Hectare','Shift','Date','Hectare Squirrel Number','Age','Primary Fur Color','Highlight Fur Color','Combination of Primary and Highlight Color','Color notes','Location','Above Ground Sighter Measurement','Specific Location','Running','Chasing','Climbing','Eating','Foraging','Other Activities','Kuks','Quaas','Moans','Tail flags','Tail twitches','Approaches','Indifferent','Runs from','Other Interactions','Lat/Long','Zip Codes','Community Districts','Borough Boundaries','City Council Districts','Police Precincts']]
+            totallen = len(data)
+            print(data[0])
+            print(data[totallen-1])
+
+            print(totallen)
             output.write(data)
             output.close()
