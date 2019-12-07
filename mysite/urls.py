aa1ef1fb3f54692cfca311c33fbcbe42558d3e32
Yanni Lu <yannilu777@gmail.com>
2019-12-07 22:18:00 +0000

    merge from feature1 branch: finish the map and export function

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('sightings/', views.all_squirrels, name='all_squirrels'),
-    path('sightings/<unique_squirrel_id>/',views.squirrel_edit, name='squirrel_edit'),
-    
+    path('sightings/', views.all_squirrels,),
+    path('sightings/<unique_squirrel_id>/',views.squirrel_edit,),
+    path('map/',views.map), 
 
 ]
 
--- mysite/views.py
+++ mysite/views.py
@@ -13,10 +13,6 @@ def all_squirrels(request):
             }
     return render(request,'mysite/sightings.html',context)
 
-def squirrel_detail(request,unique_squirrel_id):
-    s = Squirrel.objects.get(Unique_Squirrel_ID=unique_squirrel_id)
-    return HttpResponse("Hi,I'm squirrel")
-
 def map(request):
     sightings = Squirrel.objects.all()
     context = {
